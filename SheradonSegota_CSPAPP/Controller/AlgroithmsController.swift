abacd75f2cc54bbee35eb99588cc8896a71ab575
Sheradon Segota <31513874+sseg9117@users.noreply.github.com>
2017-10-26 09:58:34 -0600

    added project to gethub, and created all screens and controllers for project, and did git ignore.

---
+++ SheradonSegota_CSPAPP/Controller/AlgroithmsController.swift
@@ -0,0 +1,35 @@
+//
+//  AlgroithmsController.swift
+//  SheradonSegota_CSPAPP
+//
+//  Created by Segota, Sheradon on 10/26/17.
+//  Copyright © 2017 Segota, Sheradon. All rights reserved.
+//
+
+import UIKit
+
+class AlgroithmsController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ SheradonSegota_CSPAPP/Controller/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SheradonSegota_CSPAPP
+//
+//  Created by Segota, Sheradon on 10/26/17.
+//  Copyright © 2017 Segota, Sheradon. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
