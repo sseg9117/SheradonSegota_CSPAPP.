f26ec4435a5a7b33b4bc1e36b25f9f483caeaeed
Sheradon Segota <31513874+sseg9117@users.noreply.github.com>
2017-12-13 08:31:34 -0700

    addded a little code to startscenmene (touchnode).

--- SheradonSegota_CSPAPP/Controller/GameController/StartScene.swift
+++ SheradonSegota_CSPAPP/Controller/GameController/StartScene.swift
@@ -27,7 +27,17 @@ public class StartScene : SKScene
     
     override public func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?)
     {
+        let currentTouch = touches.first
+        let touchLocation = currentTouch?.location(in: self)
+        let touchedNode = self.atPoint(touchLocation!)
         
+        if (TouchedNode.name == "start")
+        {
+            let gameOverScene = GameScene(size: size)
+            gameOverScene.scaleMode = scaleMode
+            let transitionType = SKTransition.flipHorizontal(withDuration: 1.0)
+            view?.presentScene(gameOverScene, trasition: transitionType)
+        }
         
     }
 }
